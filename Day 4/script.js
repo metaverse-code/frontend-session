cb5f243402c269c245f882d04453f0306b25285e
metaverse-code <meta.niraj@gmail.com>
2024-07-14 22:14:37 +0530

    Session WIP

---
+++ Day 4/script.js
@@ -0,0 +1,7 @@
+function changeText() {
+    document.querySelector("h1").innerText = "Hello, Internal JavaScript!";
+}
+
+// setInterval(() => {
+//     document.querySelector('h1').innerText="Hello There";
+// }, 5000);
