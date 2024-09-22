efcf14c43d126d6ae3bee6c6e73d193542539e76
metaverse-code <meta.niraj@gmail.com>
2024-09-22 22:34:12 +0530

    JS Module

---
+++ Day21/JS/file1.js
@@ -0,0 +1,16 @@
+// function hello(){
+//     console.log("This is hello function");
+// }
+
+
+import hey1, { name, helloFile2, myVal as someNewVal} from "./file2.js";
+
+console.log(name);
+console.log(helloFile2());
+
+console.log(someNewVal);
+console.log(hey1());
+
+
+
+
---
+++ Day21/JS/file2.js
@@ -0,0 +1,17 @@
+export function helloFile2(){
+    // console.log("This is hello function file 2");
+    return {
+        name: "Amit",
+        city: "Delhi"
+    }
+}
+
+function hey(){
+    return "Hey Function";
+}
+
+export let name = "Amit kumar verma";  //named export
+
+export const myVal = "123Hello There";
+
+export default hey;
